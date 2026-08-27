dc3b45bddb6013ffbed37d937cc4e83417854167
SAHIL KHAN <146698424+Sksahilkhan67sh@users.noreply.github.com>
2026-08-27 15:32:49 +0530

    Refactor comment for logo component

--- apps/web/components/ui/logo.tsx
+++ apps/web/components/ui/logo.tsx
@@ -1,5 +1,6 @@
-// eslint-disable-next-line @next/next/no-img-element -- static /public asset, not an
-// optimizable remote image; plain <img> avoids needing next/image config for this.
+// Static /public asset, not an optimizable remote image; plain <img> avoids needing
+// next/image config for this.
 export function RelayHubMark({ size = 22 }: { size?: number }) {
+  // eslint-disable-next-line @next/next/no-img-element
   return <img src="/logo.png" alt="RelayHub" width={size} height={size} style={{ objectFit: "contain" }} />;
 }
